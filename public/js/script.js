1d2e8b6a7ee46bdd8f263b3ae4c0ee891943959f
Mahmoud Zakaria <m.zakria90@gmail.com>
2016-11-03 17:31:08 +0200

    checked the copy to clipboard

--- public/js/script.js
+++ public/js/script.js
@@ -201,8 +201,15 @@ window.addEventListener('load',function(){
 	gv.dropdown('click');
 })
 
-
-
+//Copy
+if(document.querySelector('footer button')){
+var copyBtn = document.querySelector('footer button');
+	copyBtn.onclick = function(){
+	 	var x = document.getElementsByTagName('textarea')[0]
+	 	x.select()
+	 	document.execCommand('copy')
+	}
+}
 
 
 },{"./give.js":1}]},{},[2]);
